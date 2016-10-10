25f941f51edbcd37bdaf83cb613591c6ff188cbc
Adam Bergman <adam@fransvilhelm.com>
2016-10-10 22:52:26 +0200

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <div className="container">Hello world!</div>,
   document.getElementById('root')
 );
