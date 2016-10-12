d10d9cf9c8c64bf901626a008a5cf7812c7670f4
Adam Bergman <adam@fransvilhelm.com>
2016-10-13 00:05:44 +0200

    APPLYING styles

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import getContent from '../../lib/getContent';
 import deltaToReact from '../../lib/deltaToReact';
-import getRandomInt from '../../lib/getRandomInt';
 
 import Spotify from '../Spotify';
 
@@ -9,29 +8,30 @@ import akvarell1 from '../../img/akvarell-1.png';
 import akvarell2 from '../../img/akvarell-2.png';
 import './styles.css';
 
-export default ({ pathname }) => {
+export default ({ pathname, reveal }) => {
   const { uri, ops, layout } = getContent(pathname);
 
   const sectionTextContent = deltaToReact({ ops });
 
   const sectionSidebar = () => {
     const images = [akvarell1, akvarell2];
-    const randomInt = getRandomInt(0, images.length - 1);
+    const int = pathname === '/aktuellt' ? 0 : 1;
 
     return (
       <div className="section-sidebar section-column">
         {uri ?
           <Spotify uri={uri} /> :
-          <img className="section-img" src={images[randomInt]} alt="Bild av akvarell" />
+          <img className="section-img" src={images[int]} alt="Bild av akvarell" />
         }
       </div>
     );
   };
 
   const sectionClassName = layout.reverse ? 'section section-reverse' : 'section';
+  const sectionOpacity = reveal ? Number(reveal.toFixed(2)) : 1;
 
   return (
-    <div className={sectionClassName}>
+    <div className={sectionClassName} style={{ opacity: sectionOpacity }}>
       {sectionSidebar()}
       <div className="section-text section-column">
         {sectionTextContent}
