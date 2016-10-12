ee52992528df9eea5db89de6eb308aeb56cb3799
Adam Bergman <adam@fransvilhelm.com>
2016-10-13 00:05:29 +0200

    MINOR update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Match } from 'react-router';
 
-import routes from './routes';
 import Menu from './components/Menu';
+import routes from './routes';
+
 import bg from './img/bg.jpg';
 import './index.css';
 
@@ -13,9 +14,7 @@ ReactDOM.render(
   <BrowserRouter basename={basename}>
     <div className="container" style={{ backgroundImage: `url(${bg})`}}>
       <Menu />
-      {routes.map((route, i) => (
-        <Match key={i} {...route} />
-      ))}
+      {routes.map((route, i) => <Match key={i} {...route} />)}
     </div>
   </BrowserRouter>,
   document.getElementById('root')
