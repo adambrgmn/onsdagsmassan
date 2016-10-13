04b2c45c0bc3907b119c9dd4517bfc9d25ca98cf
Adam Bergman <adam@fransvilhelm.com>
2016-10-13 22:27:19 +0200

    REFACTORING scrollTo function

--- src/components/ScrollLink/index.js
+++ src/components/ScrollLink/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+const requestAnimationFrame = (callback) => {
+  const animationFrameFallback = (cb) => setTimeout(cb, 1000 / 60)
+
+  return window.requestAnimationFrame(callback) ||
+    window.webkitRequestAnimationFrame(callback) ||
+    window.mozRequestAnimationFrame(callback) ||
+    window.msRequestAnimationFrame(callback) ||
+    window.oRequestAnimationFrame(callback) ||
+    animationFrameFallback(callback);
+}
+
 export default class ScrollLink extends Component {
   constructor(props) {
     super(props);
     this.state = { node: undefined };
 
     this.onClick = this._onClick.bind(this);
+    this.scrollTo = this._scrollTo.bind(this);
   }
 
   componentDidMount() {
@@ -21,39 +33,33 @@ export default class ScrollLink extends Component {
     return this.setState({ node: { offsetTop: 0 } });
   }
 
-  scrollTo(to, duration) {
-    const scrollFallback = (cb) => setTimeout(cb, 1000 / 60);
-    const scroll = window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.msRequestAnimationFrame ||
-      window.oRequestAnimationFrame ||
-      scrollFallback;
+  _scrollTo(to, duration) {
+    const startPoint = document.body.scrollTop;
+    const distance = to - startPoint;
+    const startTime = Date.now();
+    const endTime = startTime + duration;
 
-    const from = document.body.scrollTop;
-    const distanceToScroll = to - from;
-    const isNegativeScroll = distanceToScroll < 0;
-    const step = Math.round(distanceToScroll / (duration / 60));
-
-    const scrollWindow = () => {
-      const currentPos = document.body.scrollTop;
-      if (
-        (isNegativeScroll && currentPos <= to) ||
-        (!isNegativeScroll && currentPos >= to)
-      ) {
-        return false;
-      }
-
-      document.body.scrollTop = currentPos + step;
-      return scroll(scrollWindow);
+    const smoothStep = (start, end, now) => {
+      const x = (now - start) / (end - start);
+      return x * x * (3 - 2 * x);
     };
 
-    scrollWindow();
+    const scrollFrame = () => {
+      const now = Date.now();
+      if (now > endTime) return null;
+
+      const pointToGo = Math.round(startPoint + (distance * smoothStep(startTime, endTime, now)));
+      document.body.scrollTop = pointToGo;
+
+      return requestAnimationFrame(scrollFrame);
+    };
+
+    return requestAnimationFrame(scrollFrame);
   }
 
   _onClick(e) {
     e.preventDefault();
-    if (this.state.node) this.scrollTo(this.state.node.offsetTop, 500);
+    if (this.state.node) this.scrollTo(this.state.node.offsetTop, 2000);
   }
 
   render() {
