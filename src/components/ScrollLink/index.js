b83d60baa70d18105be00561ca244a3a9ed1c58f
Adam Bergman <adam@fransvilhelm.com>
2016-10-14 01:32:37 +0200

    REFACTORING scroll method
    Now using rebound-js

--- src/components/ScrollLink/index.js
+++ src/components/ScrollLink/index.js
@@ -1,65 +1,69 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-
-const requestAnimationFrame = (callback) => {
-  const animationFrameFallback = (cb) => setTimeout(cb, 1000 / 60)
-
-  return window.requestAnimationFrame(callback) ||
-    window.webkitRequestAnimationFrame(callback) ||
-    window.mozRequestAnimationFrame(callback) ||
-    window.msRequestAnimationFrame(callback) ||
-    window.oRequestAnimationFrame(callback) ||
-    animationFrameFallback(callback);
-}
+import { SpringSystem } from 'rebound';
 
 export default class ScrollLink extends Component {
   constructor(props) {
     super(props);
-    this.state = { node: undefined };
+    this.state = {
+      isRelativeLink: this.props.to.indexOf('/') > -1,
+    };
 
     this.onClick = this._onClick.bind(this);
-    this.scrollTo = this._scrollTo.bind(this);
+    this.initSpring = this._initSpring.bind(this);
   }
 
   componentDidMount() {
     const { to } = this.props;
+    const { isRelativeLink } = this.state;
 
-    if (to.indexOf('/') > -1) return this.setState({ node: undefined });
+    if (isRelativeLink) return this.setState({ node: undefined });
 
     const id = to.replace(/^(#)/, '');
-    const node = document.getElementById(id);
+    const node = document.getElementById(id) || { offsetTop: 0 };
 
-    if (node) return this.setState({ node });
-    return this.setState({ node: { offsetTop: 0 } });
+    return this.setState({ node }, this.initSpring);
   }
 
-  _scrollTo(to, duration) {
-    const startPoint = document.body.scrollTop;
-    const distance = to - startPoint;
-    const startTime = Date.now();
-    const endTime = startTime + duration;
-
-    const smoothStep = (start, end, now) => {
-      const x = (now - start) / (end - start);
-      return x * x * (3 - 2 * x);
-    };
-
-    const scrollFrame = () => {
-      const now = Date.now();
-      if (now > endTime) return null;
-
-      const pointToGo = Math.round(startPoint + (distance * smoothStep(startTime, endTime, now)));
-      document.body.scrollTop = pointToGo;
-
-      return requestAnimationFrame(scrollFrame);
-    };
-
-    return requestAnimationFrame(scrollFrame);
+  _initSpring() {
+    this.springSystem = new SpringSystem();
+    this.spring = this.springSystem.createSpring(10, 4);
   }
 
   _onClick(e) {
     e.preventDefault();
-    if (this.state.node) this.scrollTo(this.state.node.offsetTop, 500);
+    if (!this.spring) return null;
+
+    const { body } = document;
+    const { node } = this.state;
+
+    const startPos = body.scrollTop;
+    const endPos = node.offsetTop;
+    const diff = endPos - startPos;
+
+    const scrollTo = (val) => {
+      const newPos = Math.round(startPos + (diff * val));
+      body.scrollTop = newPos;
+
+      if (this.spring.isAtRest()) {
+        this.spring.removeAllListeners();
+        this.spring.setEndValue(0);
+      }
+    };
+
+    this.spring.addListener({
+      onSpringUpdate: (spring) => {
+        const val = spring.getCurrentValue();
+        scrollTo(val);
+
+        if (this.spring.isAtRest()) {
+          this.spring.removeAllListeners();
+          this.spring.setEndValue(0);
+        }
+      }
+    });
+
+    return this.spring.setEndValue(1);
   }
 
   render() {
