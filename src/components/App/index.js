aed73dfb76c4415229558ecdbda6bf4641022653
Adam Bergman <adam@fransvilhelm.com>
2016-11-07 23:44:28 +0100

    EXTRACTED content fetching to Section

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,95 +1,37 @@
 // @flow
 
-import React, { Component } from 'react';
-import getContent from '../../lib/getContent';
+import React from 'react';
 
 import Menu from '../Menu';
 import Home from '../Home';
 import Footer from '../Footer';
 import Section from '../Section';
 
-type SectionContent = {
-  title: string;
-  body: string;
-  uri: string;
-}
+export default function App() {
+  const routes = [
+    { path: '', title: 'Hem' },
+    { path: 'aktuellt', title: 'Aktuellt' },
+    { path: 'information', title: 'Information' },
+    { path: 'musik', title: 'Musik' },
+  ];
 
-type Route = {
-  path: string;
-  title: string;
-}
+  return (
+    <div className="container">
+      <Menu routes={routes} />
+      <Home />
+      {routes.slice(1).map(({ path }, i) => {
+        let reverse = false;
+        if (i % 2) reverse = true;
 
-type State = {
-  sections: SectionContent[];
-  routes: Route[];
-};
-
-export default class App extends Component {
-  state: State;
-  props: {};
-
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
-      sections: [],
-      routes: [{
-        path: '',
-        title: 'Hem',
-      }],
-    };
-  }
-
-  componentDidMount(): void {
-    this.fetchContent();
-  }
-
-  fetchContent = (): void => {
-    const sections = ['aktuellt', 'information', 'musik'];
-    const promises = sections.map((section) => getContent(section));
-
-    Promise.all(promises)
-      .then((sectionsContent) => {
-        const sortedSectionsContent = sectionsContent
-          .sort((currSection, prevSection) => (currSection.title < prevSection.title ? 0 : 1));
-
-        sortedSectionsContent.forEach((sectionContent) => {
-          const section = sectionContent;
-          const route = {
-            path: sectionContent.title.toLowerCase(),
-            title: sectionContent.title,
-          };
-
-          this.setState(() => ({
-            sections: [...this.state.sections, section],
-            routes: [...this.state.routes, route],
-          }));
-        });
-      });
-  }
-
-  render() {
-    const { routes, sections } = this.state;
-
-    return (
-      <div className="container">
-        <Menu routes={routes} />
-        <Home />
-        {sections.map((section, index) => {
-          let reverse = false;
-          if (index % 2) reverse = true;
-
-          return (
-            <Section
-              key={section.title}
-              content={section}
-              sectionName={section.title.toLowerCase()}
-              reverse={reverse}
-            />
-          );
-        })}
-        <Footer />
-      </div>
-    );
-  }
+        return (
+          <Section
+            key={path}
+            sectionName={path}
+            reverse={reverse}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,37 +1,77 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
+import getContent from '../../lib/getContent';
 
 import s from './styles.scss';
 
 import SectionText from '../SectionText';
 import SectionSidebar from '../SectionSidebar';
 
-type Content = {
-  title: string;
-  uri: string;
-  body: string;
-}
 
 type Props = {
   sectionName: string;
-  content: Content;
   reverse: boolean;
 }
 
-export default function Section(props: Props) {
-  const { sectionName, reverse } = props;
+type State = {
+  body: string;
+  title: string;
+  excerpt: string;
+  uri?: string;
+  showExcerpt: boolean;
+}
 
-  const cx = {
-    [s.section]: true,
-    [`section-${sectionName}`]: true,
-  };
+export default class Section extends Component {
+  props: Props;
+  state: State;
 
-  return (
-    <div id={sectionName} className={classNames(cx)}>
-      <SectionSidebar uri={props.content.uri} reverse={reverse} />
-      <SectionText title={props.content.title} body={props.content.body} />
-    </div>
-  );
+  constructor(props: Props) {
+    super(props);
+    this.state = { body: '', title: '', excerpt: '', showExcerpt: false };
+  }
+
+  componentDidMount(): void { this.fetchContent(); }
+
+  fetchContent = (): void => {
+    getContent(this.props.sectionName)
+      .then(({ body, title, uri }) => {
+        let showExcerpt = false;
+        const splitBody = body.split('\n\n');
+        const excerpt = splitBody
+          .slice(0, 5)
+          .join('\n\n');
+
+        if (splitBody.length > 5) showExcerpt = true;
+
+        this.setState({ body, title, uri, excerpt, showExcerpt });
+      })
+      .catch(console.error);
+  }
+
+  onShowMoreClick = (e: SyntheticEvent): void => {
+    e.preventDefault();
+    this.setState({ showExcerpt: !this.state.showExcerpt });
+  }
+
+  render() {
+    const { sectionName, reverse } = this.props;
+    const { body, title, uri, excerpt, showExcerpt } = this.state;
+    const cx = {
+      [s.section]: true,
+      [`section-${sectionName}`]: true,
+    };
+
+    return (
+      <div id={sectionName} className={classNames(cx)}>
+        <SectionSidebar uri={uri} reverse={reverse} />
+        <SectionText
+          title={title}
+          body={showExcerpt ? excerpt : body}
+          showMore={showExcerpt ? this.onShowMoreClick : undefined}
+        />
+      </div>
+    );
+  }
 }
