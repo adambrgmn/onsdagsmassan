7398343df6d1efad5a610d889a3851887f4ec2cb
Adam Bergman <adam@fransvilhelm.com>
2016-11-07 23:44:43 +0100

    ADDED show more button

--- src/components/SectionText/index.js
+++ src/components/SectionText/index.js
@@ -8,13 +8,18 @@ import s from './styles.scss';
 type Props = {
   title: string;
   body: string;
+  showMore?: (e: SyntheticEvent) => void;
 }
 
-export default function SectionText({ title, body }: Props) {
+export default function SectionText({ title, body, showMore }: Props) {
   return (
     <div className={s.sectionTextContent}>
       <h2>{title}</h2>
       <ReactMarkdown source={body} />
+      {showMore ?
+        <button onClick={showMore} type="button" className={s.sectionShowButton}>Läs mer ...</button> :
+        null
+      }
     </div>
   );
 }
