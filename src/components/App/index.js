1ec2c7d62258fd632261de5d75067b0e4d93d06d
Adam Bergman <adam@fransvilhelm.com>
2016-11-07 22:44:20 +0100

    ADDED async content fetching

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+// @flow
+
+import React, { Component } from 'react';
 import getContent from '../../lib/getContent';
 
 import Menu from '../Menu';
@@ -6,19 +8,88 @@ import Home from '../Home';
 import Footer from '../Footer';
 import Section from '../Section';
 
-export default () => {
-  const aktuellt = getContent('aktuellt');
-  const information = getContent('information');
-  const musik = getContent('musik');
+type SectionContent = {
+  title: string;
+  body: string;
+  uri: string;
+}
 
-  return (
-    <div className="container">
-      <Menu />
-      <Home />
-      <Section content={aktuellt} pathname="/aktuellt" />
-      <Section content={information} pathname="/information" />
-      <Section content={musik} pathname="/musik" />
-      <Footer />
-    </div>
-  );
+type Route = {
+  path: string;
+  title: string;
+}
+
+type State = {
+  sections: SectionContent[];
+  routes: Route[];
 };
+
+export default class App extends Component {
+  state: State;
+  props: {};
+
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      sections: [],
+      routes: [{
+        path: '',
+        title: 'Hem',
+      }],
+    };
+  }
+
+  componentDidMount(): void {
+    this.fetchContent();
+  }
+
+  fetchContent = (): void => {
+    const sections = ['aktuellt', 'information', 'musik'];
+    const promises = sections.map((section) => getContent(section));
+
+    Promise.all(promises)
+      .then((sectionsContent) => {
+        const sortedSectionsContent = sectionsContent
+          .sort((currSection, prevSection) => (currSection.title < prevSection.title ? 0 : 1));
+
+        sortedSectionsContent.forEach((sectionContent) => {
+          const section = sectionContent;
+          const route = {
+            path: sectionContent.title.toLowerCase(),
+            title: sectionContent.title,
+          };
+
+          this.setState(() => ({
+            sections: [...this.state.sections, section],
+            routes: [...this.state.routes, route],
+          }));
+        });
+      });
+  }
+
+  render() {
+    const { routes, sections } = this.state;
+
+    return (
+      <div className="container">
+        <Menu routes={routes} />
+        <Home />
+        {sections.map((section, index) => {
+          let reverse = false;
+          if (index % 2) reverse = true;
+
+          return (
+            <Section
+              key={section.title}
+              content={section}
+              sectionName={section.title.toLowerCase()}
+              reverse={reverse}
+            />
+          );
+        })}
+        <Footer />
+      </div>
+    );
+  }
+}
--- src/lib/getContent/index.js
+++ src/lib/getContent/index.js
@@ -1,3 +1,27 @@
-import content from '../../db';
+// @flow
 
-export default (path) => content[path];
+import content from '../../db.json';
+
+type Section = {
+  title: string;
+  body: string;
+  uri: string;
+}
+
+const readContent = (section: string): Promise<Section> => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const sectionContent = content[section];
+
+    if (!sectionContent) return reject(new Error(`No content found for section ${section}`));
+    return resolve(sectionContent);
+  }, 100);
+});
+
+export default async function getContent(section: string): Promise<Section> {
+  try {
+    const sectionContent = await readContent(section);
+    return sectionContent;
+  } catch (err) {
+    return err;
+  }
+}
