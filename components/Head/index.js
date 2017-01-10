6ce9c0bc3963ed23dd8ff551d026c558cb977351
Adam Bergman <adam@fransvilhelm.com>
2017-01-10 19:04:14 +0100

    UPDATE head title

--- components/Head/index.js
+++ components/Head/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 
 export default ({ children }) => (
   <Head>
-    <title>Onsdgasm&auml;ssan | kl 19.30 | St Andrew&apos;s Church</title>
+    <title>Onsdgasm&auml;ssan | kl 18.30 | Betlehemskyrkan</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta httpEquiv="Accept-CH" content="DPR, Width" />
     <link rel="shortcut icon" href="/static/favicon.ico" />
