9abc5b45c02c1d989a3a3abfab1dad260df798db
Adam Bergman <adam@fransvilhelm.com>
2017-01-11 23:08:13 +0100

    UPDATE to match new version of now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,6 @@ BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 NODE_ENV="development"
 ALIAS=${ALIAS_DEVELOPMENT}
 
-function get_id {
-  now ls onsdagsmassan -t ${NOW_TOKEN} | sed -n 5p | awk '{ print $1 }'
-}
-
 if [[ $BRANCH == 'master' ]]; then
   NODE_ENV="production"
   ALIAS=${ALIAS_PRODUCTION}
@@ -16,7 +12,4 @@ fi
 
 echo "Deploying from branch ${BRANCH}"
 echo "with NODE_ENV=${NODE_ENV}"
-now -e NODE_ENV=${NODE_ENV} -e FB_API_KEY=${FB_API_KEY} -t ${NOW_TOKEN} -C -f
-
-echo "Setting alias for deployment to ${ALIAS}"
-now alias set $(get_id) ${ALIAS} -t ${NOW_TOKEN}
+now -e NODE_ENV=${NODE_ENV} -e FB_API_KEY=${FB_API_KEY} -t ${NOW_TOKEN} -a ${ALIAS} -C -f
