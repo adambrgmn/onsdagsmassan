14df8aa83a750a350733e224c79b6d03b11fbb47
Adam Bergman <adam@fransvilhelm.com>
2017-01-11 22:48:26 +0100

    ADD support for parallax scrolling with Rellax

--- components/SlowScroll/index.js
+++ components/SlowScroll/index.js
@@ -1,61 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { aboveTablet } from '../../utils/mediaQuery';
 
-const registerEventListener = (fn) => {
-  window.addEventListener('scroll', fn);
-  return () => window.removeEventListener('scroll', fn);
+export default ({ ignoreMobile, faster, center, className, children }) => {
+  const cx = center ? 'rellax-center' : 'rellax';
+  const speed = faster ? 2 : -2;
+  const ignore = ignoreMobile && !aboveTablet();
+
+  return (
+    <div
+      className={ignore ? undefined : cx}
+      {...className}
+      data-rellax-speed={speed}
+    >
+      {children}
+    </div>
+  );
 };
-
-export default class SlowScroll extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { windowHeight: 0, error: false, translate: 0 };
-    this.ref = null;
-    this.parent = null;
-  }
-
-  componentDidMount() {
-    const { ignoreMobile } = this.props;
-    const { error } = this.state;
-
-    if ((aboveTablet() || ignoreMobile) && !error) {
-      this.onMount();
-      this.unregisterEventListener = registerEventListener(this.onScroll);
-    }
-  }
-
-  componentWillUnmount() {
-    if ('unregisterEventListener' in this) this.unregisterEventListener();
-  }
-
-  onMount = () => this.setState({ windowHeight: window.innerHeight })
-
-  onScroll = () => {
-    const translateY = (val) => (this.ref.style.transform = `translateY(${val}%)`);
-    const { windowHeight } = this.state;
-    const { maxTranslate } = this.props;
-    const parentRect = this.parent.getBoundingClientRect();
-
-    const slowScroll = () => {
-      const distanceFromTop = parentRect.top / windowHeight;
-      translateY(-maxTranslate * distanceFromTop);
-    };
-
-    if (parentRect.top > windowHeight || parentRect.bottom < 0) return null;
-    return window.requestAnimationFrame(slowScroll);
-  }
-
-  setRef = (ref) => {
-    try {
-      this.ref = ref;
-      this.parent = ref.parentNode.parentNode;
-    } catch (error) {
-      this.setState({ error });
-    }
-  }
-
-  render() {
-    const { className, children } = this.props;
-    return (<div {...className} ref={this.setRef}>{children}</div>);
-  }
-}
