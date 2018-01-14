362e0317404dbd68207ee8a3248083d889cdc4e6
Adam Bergman <adam@fransvilhelm.com>
2018-01-14 10:30:30 +0100

    Clean up old implementation

--- utils/api/index.js
+++
@@ -1,6 +0,0 @@
-import Firebase from './firebase';
-
-const app = Firebase.of(process.env.FB_API_KEY);
-
-export const getSections = app.getSections.bind(app);
-export const getSection = app.getSection.bind(app);
--- utils/mediaQuery/index.js
+++
@@ -1,6 +0,0 @@
-import { media } from '../../styles/variables';
-
-const windowDefined = () => typeof window !== 'undefined';
-export const createMediaQuery = (query) => () => windowDefined() && window.innerWidth >= query;
-export const aboveTablet = createMediaQuery(media.tablet);
-export const aboveDesktop = createMediaQuery(media.desktop);
