76e888c8fced44ce1d1f750e70c2d9f8f72bfc0f
Adam Bergman <adam@fransvilhelm.com>
2018-01-15 10:06:12 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello world');
+import scrollDir from './utils/scroll-dir';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const nav = document.querySelector('.nav');
+  scrollDir(nav);
+});
